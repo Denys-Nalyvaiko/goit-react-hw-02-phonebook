2b3ba4a5ba416877e7ebe81b86be47353ad3042f
Denys-Nalyvaiko <nalyvaikodenys@gmail.com>
2023-11-13 01:10:57 +0100

    add App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,76 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+
+    const { name, number } = this.state;
+    const currentContact = { id: nanoid(), name: name, number: number };
+
+    this.setState(prevState => ({
+      contacts: [currentContact, ...prevState.contacts],
+    }));
+
+    this.reset();
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    const nameInputId = nanoid();
+    const numberInputId = nanoid();
+
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <form action="submit" onSubmit={this.handleFormSubmit}>
+          <label htmlFor={nameInputId} />
+          Name
+          <input
+            type="text"
+            name="name"
+            id={nameInputId}
+            value={this.state.name}
+            required
+            onChange={this.handleInputChange}
+          />
+          <label htmlFor={numberInputId} />
+          Number
+          <input
+            type="tel"
+            name="number"
+            id={nameInputId}
+            value={this.state.number}
+            required
+            onChange={this.handleInputChange}
+          />
+          <button type="submit">Add contact</button>
+        </form>
+        <h2>Contacts</h2>
+        <ul>
+          {this.state.contacts.map(({ id, name, number }) => (
+            <li key={id}>
+              <p>
+                {name}: {number}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
