fc72f177ce464937f7c8b747e442d1c36a2fdf5c
Denys-Nalyvaiko <nalyvaikodenys@gmail.com>
2023-11-14 13:51:20 +0100

    add styles to contact list

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,7 +1,29 @@
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import FolderIcon from '@mui/icons-material/Folder';
+import DeleteIcon from '@mui/icons-material/Delete';
+
 export const ContactItem = ({ name, number }) => (
-  <li>
-    <p>
-      {name}: {number}
-    </p>
-  </li>
+  <ListItem
+    secondaryAction={
+      <IconButton edge="end" aria-label="delete">
+        <DeleteIcon color="primary" />
+      </IconButton>
+    }
+  >
+    <ListItemAvatar>
+      <Avatar sx={{ backgroundColor: '#f6d9b1', color: '#252525' }}>
+        <FolderIcon />
+      </Avatar>
+    </ListItemAvatar>
+    <ListItemText
+      primary={name}
+      secondary={number}
+      primaryTypographyProps={{ color: '#f6d9b1' }}
+      secondaryTypographyProps={{ color: '#dca75d' }}
+    />
+  </ListItem>
 );
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,9 +1,19 @@
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import Grid from '@mui/material/Grid';
+import { Demo } from './ContactListDemo.styled';
 import { ContactItem } from 'components/ContactItem/ContactItem';
 
 export const ContactList = ({ contacts }) => (
-  <ul>
-    {contacts.map(({ id, name, number }) => (
-      <ContactItem key={id} name={name} number={number} />
-    ))}
-  </ul>
+  <Box sx={{ flexGrow: 1, maxWidth: 752 }}>
+    <Grid item xs={12} md={6}>
+      <Demo>
+        <List>
+          {contacts.map(({ id, name, number }) => (
+            <ContactItem key={id} name={name} number={number} />
+          ))}
+        </List>
+      </Demo>
+    </Grid>
+  </Box>
 );
